1c0f51c393b89439ff414c44082031aacad205b4
Oluwatosin Akande <aktosin1@gmail.com>
2023-12-25 17:08:47 +0100

    update: set up cicd for serverless deploy

--- src/handler.js
+++ src/handler.js
@@ -1,18 +1,4 @@
-'use strict';
+const serverless = require('serverless-http');
+const app = require('./app');
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+module.exports.handler = serverless(app);
